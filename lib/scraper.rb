4fdb45be0f72e7ac34edf4afcb2ee7f7eada8c82
Yisrael Gurkow <yisrelg3@gmail.com>
2021-06-22 00:11:19 -0400

    commit tree 72bdfeb23afab9b28636325069a74f63d7e53ed7

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = URI.open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+# doc.search(".headline-26OIBN")
+courses =  doc.search(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0]
+puts courses
+# courses.map do |course|
+#     puts course.text.strip
+# end
